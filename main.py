7ed3585c4232a38892f9ccf722febdb51b882abc
Philip Lee <phil1999@uw.edu>
2020-03-13 17:08:28 -0700

    Created starter files
    add starter files

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+NETID: phil1999
+Philip Lee
+Section AA
+
+
+TODO
+"""
+# Analysis
+import pandas as pd
+import numpy as np
+# Plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+path = '../datasets/coronavirus.csv'
+
+
+def load_data(shapefile):
+    pass
+
+
+def plotMap():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
